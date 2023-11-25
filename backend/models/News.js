1200502981f36735777e9564292a18c78049144c
w3hhh.m <w3hhh.m@gmail.com>
2023-11-25 20:26:56 +0300

    FUCK MARK AAA BLYAAAT

--- backend/models/News.js
+++ backend/models/News.js
@@ -1,2 +1,24 @@
+const mongoose = require("mongoose");
+
+const newsSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  organization:{
+    type: String,
+    required: true,
+  }
+});
+
+const News = mongoose.model("News", newsSchema);
 
 module.exports = News;
