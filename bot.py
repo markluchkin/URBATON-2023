02e2ef9adc399f2d1eab1e15311ce5e10404d19c
rnymphaea <strangeisdoctor@gmail.com>
2023-11-26 01:09:21 +0300

    some big changes

--- bot.py
+++ bot.py
@@ -1,42 +1,32 @@
 import asyncio
 import pymongo
-from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+import time
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.dispatcher.filters import Command, CommandStart
 from config import TOKEN, DB_URL
 
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher()
+dp = Dispatcher(bot)
 CLUSTER = pymongo.MongoClient(DB_URL)
 DB = CLUSTER["test"]
+organization = ""
+chat_id = 0
+
+async def get_action_keyboard():
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = [KeyboardButton(text="Расписание на всю неделю"), KeyboardButton(text="Расписание на день")]
+    keyboard.add(*buttons)
+    return keyboard
 
 
-@dp.message(CommandStart())
-async def start(message: Message):
+@dp.message_handler(CommandStart())
+async def start(message: types.Message):
     user_full_name = message.from_user.full_name
     await message.answer(f"Здравствуйте, {user_full_name}. \n"
                          f"Чтобы продолжить, введите: /auth <ваш логин> <ваш пароль>. \n"
-                         f"Пример: /auth qwerty@gmail.com qwerty123")
-
-
-@dp.message(Command('auth'))
-async def authorize(message: Message):
-    text = message.text.split()
-    login, password = text[1], text[2]
-    check = check_user(DB, login, password)
-    if check:
-        await message.answer(text="Авторизация прошла успешно")
-        print(check[0]['organization'])
-        is_user_authorized = True
-    else:
-        await message.answer(text="Не удалось авторизоваться")
-
-
-async def main() -> None:
-    # Initialize Bot instance with a default parse mode which will be passed to all API calls
-    # And the run events dispatching
-    await dp.start_polling(bot)
+                         f"Пример: /auth qwerty@gmail.com qwerty123",)
 
 
 def check_user(db, login, password):
@@ -48,6 +38,43 @@ def check_user(db, login, password):
     return
 
 
+def check_news(db, organization, chat_id):
+    news = []
+    for i in db['news'].find({'organization': organization}):
+        if chat_id not in i['sentTo']:
+            news.append(i)
+    db['news'].update_many({'organization': organization}, {"$push": {"sentTo": chat_id}})
+    return news
+
+
+@dp.message_handler(Command('auth'))
+async def authorize(message: types.Message):
+    global chat_id
+    global organization
+    chat_id = message.chat.id
+    text = message.text.split()
+    login, password = text[1], text[2]
+    check = check_user(DB, login, password)
+    if check:
+        await message.answer(text="Авторизация прошла успешно", reply_markup=await get_action_keyboard())
+        organization = check[0]['organization']
+
+    else:
+        await message.answer(text="Не удалось авторизоваться")
+
+
+@dp.message_handler(Command("go"))
+async def send_news(message):
+    while True:
+        news = check_news(DB, organization, chat_id)
+        for i in news:
+            await bot.send_message(chat_id, text=f"*{i['title']}*\n"
+                                      f"{i['text']}", parse_mode="Markdown")
+        time.sleep(30)
+
+
+async def main() -> None:
+    await dp.start_polling()
+
 if __name__ == '__main__':
     asyncio.run(main())
-
--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = '6875301246:AAG1iF7Hf_zTGy5X872Ewly8YkZyTbBpH54'
+TOKEN = '6855855381:AAFUjiugz6Ub9ZlkB7H9SkAaG00OKFedr64'
 DB_URL = 'mongodb+srv://admin:admin123@test.ivw9qsv.mongodb.net/?retryWrites=true&w=majority'
