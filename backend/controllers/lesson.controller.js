04ec0c6b10f3d56130679f272419b38d26516bb3
w3hhh.m <w3hhh.m@gmail.com>
2023-11-26 13:04:39 +0300

    nice returns of all get lessons

--- backend/controllers/lesson.controller.js
+++ backend/controllers/lesson.controller.js
@@ -5,8 +5,14 @@ const Student = require("../models/Student");
 
 const getStudLess = async (studentId) => {
   try {
-    const student = await Student.findById(studentId).populate("timetable");
-
+    //const student = await Student.findById(studentId).populate("timetable");
+    const student = await Student.findById(studentId).populate({
+      path: "timetable",
+      populate: {
+        path: "teacher", // assuming "group" is the field name in the Lesson model
+        model: "Teacher", // replace with your actual model name for groups
+      },
+    });
     if (!student) {
       throw new Error("Студент не найден");
     }
@@ -18,7 +24,7 @@ const getStudLess = async (studentId) => {
       acc[lesson.day].push({
         time: lesson.time,
         subject: lesson.subject,
-        teacher: lesson.teacher,
+        teacher: lesson.teacher.name,
         cabinet: lesson.cabinet,
       });
       return acc;
@@ -26,7 +32,7 @@ const getStudLess = async (studentId) => {
 
     for (const day in groupedByDay) {
       result.push({
-        day: parseInt(day),
+        day: day,
         lessons: groupedByDay[day],
       });
     }
@@ -37,10 +43,84 @@ const getStudLess = async (studentId) => {
     throw error;
   }
 };
+const getTeachLess = async (teacherId) => {
+  try {
+    const teacher = await Teacher.findById(teacherId).populate({
+      path: "timetable",
+      populate: {
+        path: "group", // assuming "group" is the field name in the Lesson model
+        model: "Group", // replace with your actual model name for groups
+      },
+    });
 
+    if (!teacher) {
+      throw new Error("Преподаватель не найден");
+    }
+
+    const result = [];
+
+    const groupedByDay = teacher.timetable.reduce((acc, lesson) => {
+      console.log(lesson);
+      acc[lesson.day] = acc[lesson.day] || [];
+      acc[lesson.day].push({
+        time: lesson.time,
+        subject: lesson.subject,
+        student: lesson.student,
+        group: lesson.group.name,
+        cabinet: lesson.cabinet,
+      });
+      return acc;
+    }, {});
+
+    for (const day in groupedByDay) {
+      result.push({
+        day: day,
+        lessons: groupedByDay[day],
+      });
+    }
+
+    return result;
+  } catch (error) {
+    console.error("Ошибка при получении уроков преподавателя:", error.message);
+    throw error;
+  }
+};
+const getAllLess = async () => {
+  try {
+    const allLessons = await Lesson.find({})
+      .sort({ day: 1, time: 1 }) // Sort by day and then time
+      .populate("teacher")
+      .populate("group");
+
+    const result = [];
+
+    const groupedByDay = allLessons.reduce((acc, lesson) => {
+      const dayKey = lesson.day.toString();
+      acc[dayKey] = acc[dayKey] || { day: lesson.day, lessons: [] };
+      acc[dayKey].lessons.push({
+        time: lesson.time,
+        subject: lesson.subject,
+        teacher: lesson.teacher.name,
+        group: lesson.group.name,
+        cabinet: lesson.cabinet,
+      });
+      return acc;
+    }, {});
+
+    // Convert the object back to an array
+    for (const dayKey in groupedByDay) {
+      result.push(groupedByDay[dayKey]);
+    }
+
+    return result;
+  } catch (error) {
+    console.error("Ошибка при получении всех уроков:", error.message);
+    throw error;
+  }
+};
 
 class lessonController{
-  async createLesson(req, res){
+async createLesson(req, res){
     try{
       const userRole = req.user.role;
       // добавить проверку на пустоту
@@ -87,9 +167,14 @@ class lessonController{
     }
 }
 async getTeacherLessons(req, res){
-
+  try {
+    const teacherId = req.body.teacherId;
+    const lessons = await getTeachLess(teacherId);
+    res.json({ lessons });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
-
 async getStudentLessons(req,res){
   try {
     const studentId = req.body.studentId;
@@ -99,6 +184,15 @@ async getStudentLessons(req,res){
     res.status(500).json({ error: error.message });
   }
 }
+async getAllLessons(req,res){
+  try {
+    const lessons = await getAllLess();
+    res.json({ lessons });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
 }
 
 module.exports = new lessonController();
--- backend/routes/lesson.routes.js
+++ backend/routes/lesson.routes.js
@@ -4,7 +4,8 @@ const { check } = require("express-validator");
 const router = new Router();
 
 router.post('/createLesson', lessonController.createLesson);
-router.get('/getTeacherLesson', lessonController.getTeacherLessons);
+router.post('/getTeacherLesson', lessonController.getTeacherLessons);
 router.post('/getStudentLesson', lessonController.getStudentLessons);
+router.get('/getAllLesson', lessonController.getAllLessons);
 
 module.exports = router;
